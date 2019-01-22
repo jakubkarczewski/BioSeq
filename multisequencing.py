6f33876e71406252b17ae224f42bac29066eae37
jakubkarczewski <jakub.karczewski@gmail.com>
2019-01-22 16:40:12 +0100

    Final changes

--- multisequencing.py
+++ multisequencing.py
@@ -1,55 +1,46 @@
-"""
-Zestawienia wielu sekwencji. Typ A.
-Dla zadanego wielodopasowania ciągów
-nukleotydowych i macierzy podobieństwa wyznaczyć macierz profilu i słowo konsensusowe.
+from os.path import isfile
 
-Dla dwóch wielodopasowań:
-- zestawić je w jedno przez złożenie (optymalne, globalne, bez
-funkcji kary za przerwy, macierz podobieństwa liter jest dana) ich profili.
-
-Wreszcie:
- - progressive multialigning podanych sekwencji w jedno wielodopasowanie poprzez
-klasteryzację i zestawienie profili. Strategia wyboru sklejanych pod-dopasowań: dowolna
-prosta np. UPGMA (tj. guide tree nie wymagane).
-"""
-import os
-from os.path import join, isfile
-
-import numpy as np
 from Bio import AlignIO
 from Bio.Align.Applications import ClustalwCommandline
-from Bio.Alphabet import generic_dna
-from Bio.SeqRecord import SeqRecord
-from Bio.Seq import Seq
-from Bio.Align import MultipleSeqAlignment, AlignInfo
+from Bio.Align import AlignInfo
 
-from sequencing import Aligner
 
 class MultiAligner:
-    def __init__(self, seq_paths, clustal_path="./clustalw2"):
+    def __init__(self, seq_paths, gap_penalty, custom_matrix, matrix_path,  clustal_path="./clustalw2"):
         for path in seq_paths:
             assert isfile(path)
         assert isfile(clustal_path)
         self.clustal_path = clustal_path
         self.seqs_paths = seq_paths
+
         self.seqs = {'seqA': None, 'seqB': None}
         self.alignments = {'seqA': None, 'seqB': None}
+        self.gap_penalty = gap_penalty
+        self.custom_matrix = custom_matrix
+
+        assert isfile(matrix_path)
+        self.matrix_path = matrix_path
 
     def load_files(self):
-        """Opens input file and loads it's content."""
+        """Opens input files and loads their content."""
         for path in self.seqs_paths:
             seq_name = path.split('.')[0]
             self.seqs[seq_name] = AlignIO.parse(path, "fasta")
 
-    def get_alignments(self, verbose=False):
+    def get_alignments(self):
+        """Performs multialignment for sequences in each input file."""
         for path in self.seqs_paths:
-            cline = ClustalwCommandline(self.clustal_path, infile=path)
+            # set command parameters for clustalw2 execution
+            args = [self.clustal_path]
+            kwargs = {'infile': path}
+            if self.custom_matrix:
+                kwargs['transweight'] = 0
+                kwargs['matrix'] = self.matrix_path
+            # execute command
+            cline = ClustalwCommandline(*args, **kwargs)
             stdout, stderr = cline()
-            if verbose:
-                if not stderr:
-                    print(stdout)
-                else:
-                    print(stderr)
+
+            print(stdout, '\n', stderr)
 
     @staticmethod
     def normalize(one_dict):
@@ -60,43 +51,57 @@ class MultiAligner:
         return rv
 
     def process_matrix(self, pssm):
+        """Normalizes PSSM matrix."""
         processed_list = []
         for i, elem in enumerate(pssm):
             processed_list.append((str(i), self.normalize(elem[-1])))
         return processed_list
 
     def get_profile_matrix(self):
+        """Prints PSSM matrix, consensus word."""
         for path in self.seqs_paths:
             filename = path.split('.')[0] + ".aln"
             alignment = AlignIO.read(filename, 'clustal')
+            # get consensus word
             summary_align = AlignInfo.SummaryInfo(alignment)
             consensus = summary_align.dumb_consensus()
+            # get pssm matrix and process it
             my_pssm = summary_align.pos_specific_score_matrix(consensus)
             copy = my_pssm.pssm
             processed = self.process_matrix(copy)
             my_pssm.pssm = processed
+            # print the data
             print('For', path.split('.')[0], ':')
             print("Profile matrix:\n", my_pssm)
             print("Consensus word:\n", consensus)
 
-    def get_profile_alignment(self):
+    def get_slow_alignment(self):
+        """Performs slow (non-progressive) multialignment."""
         filenames = [base + '.aln' for base in self.seqs.keys()]
         assert len(filenames) == 2
-        cline = ClustalwCommandline(self.clustal_path, profile1=filenames[0], profile2=filenames[1])
+        # set command parameters for clustalw2 execution
+        args = [self.clustal_path]
+        kwargs = {'profile1': filenames[0], 'profile2': filenames[1]}
+        kwargs['pwgapopen'] = 5 if self.gap_penalty else 0
+        kwargs['pwgapext'] = 5 if self.gap_penalty else 0
+        if self.custom_matrix:
+            kwargs['pwmatrix'] = self.matrix_path
+        # execute command
+        cline = ClustalwCommandline(*args, **kwargs)
         stdout, stderr = cline()
 
-        print(stderr, '\n', stdout)
+        print(stdout, '\n', stderr)
 
+    def get_progressive_profile_alignment(self):
+        args = [self.clustal_path]
+        kwargs = {'profile1': self.seqs_paths[0], 'profile2': self.seqs_paths[1]}
+        kwargs['gapopen'] = 5 if self.gap_penalty else 0
+        kwargs['gapext'] = 5 if self.gap_penalty else 0
+        if self.custom_matrix:
+            kwargs['transweight'] = 0
+            kwargs['matrix'] = self.matrix_path
 
+        cline = ClustalwCommandline(*args, **kwargs)
+        stdout, stderr = cline()
 
-
-    # @staticmethod
-    # def _preprocess(seqs):
-    #     preprocessed = []
-    #     for seq in seqs:
-    #         preprocessed.append(SeqRecord(Seq(seq, generic_dna)))
-    #     return preprocessed
-    #     # return [SeqRecord(Seq(seq, generic_dna)) for seq in seqs]
-    #
-    # def multiple_alignment(self):
-    #     self.alignments = MultipleSeqAlignment(self._preprocess(self.seqs))
+        print(stdout, '\n', stderr)
--- run_multisequencing.py
+++ run_multisequencing.py
@@ -8,21 +8,23 @@ if __name__ == "__main__":
                         help="Path to .fasta file.")
     parser.add_argument("--seqB_path", type=str, required=True,
                         help="Path to .fasta file.")
-    parser.add_argument("--aligner_mode", type=str, default='global',
-                        help="local or global.")
-    parser.add_argument("--distance_matrix", type=str, default='distance_matrix.csv',
+    parser.add_argument("--distance_matrix", type=str, default='./clustal_matrix',
                         help="Distance matrix path.")
+    parser.add_argument("--gap_penalty", type=bool, default=True,
+                        help="True if gap penalty enabled.")
+    parser.add_argument("--custom_matrix", type=bool, default=True,
+                        help="True if want to use custom matrix")
     args = parser.parse_args()
 
-    multialigner = MultiAligner([args.seqA_path, args.seqB_path])
+    multialigner = MultiAligner([args.seqA_path, args.seqB_path], args.gap_penalty, args.custom_matrix,
+                                args.distance_matrix)
 
     multialigner.load_files()
 
     multialigner.get_alignments()
 
     multialigner.get_profile_matrix()
+    #
+    # multialigner.get_slow_alignment()
 
-    multialigner.get_profile_alignment()
-
-    print("debug")
-
+    # multialigner.get_progressive_profile_alignment()
